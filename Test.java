38dc15132f12ca972fed1088eba7fc5ae4375ca5
ektha <ekthakoribilli13@gmail.com>
2026-09-05 11:22:05 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hey, this is Java!");
+        System.out.println("Java program executed successfully through Jenkins!");
+    }
+}
